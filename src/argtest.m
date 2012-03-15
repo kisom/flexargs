d8a17c3f897eb7f8915c528cff8dbd016252fa58
Kyle Isom <coder@kyleisom.net>
2012-03-15 10:07:35 +0300

    add some useful output to argtest

--- src/argtest.m
+++ src/argtest.m
@@ -13,9 +13,21 @@
 
 int main(int argc, char *argv[])
 {
+    NSLog(@"number of args: %d\n", argc);
+
+    // skip the binary name
+    argc--;
+    argv++;
+    NSLog(@"actual number of args: %d\n\n", argc);
+
+    NSLog(@"argument list:\n");
+    for (int i = 0; i < argc; ++i)
+        NSLog(@"arg %d: %s\n", argc, argv[i]);
+
     @autoreleasepool {
         FlexArgs *flexargs = [[FlexArgs alloc] initParser:argv nargs:argc];
         NSDictionary *args = [flexargs retrieveArgs];
+        [args writeToFile:@"result.txt" atomically:true];
     }
     return 0;
 }
