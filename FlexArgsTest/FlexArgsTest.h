8368dafc38e73fc32e272510058f5940eabe1c1d
Kyle Isom <coder@kyleisom.net>
2012-03-15 17:18:25 +0300

    Adding first test case.

---
+++ FlexArgsTest/FlexArgsTest.h
@@ -0,0 +1,13 @@
+//
+//  FlexArgsTest.h
+//  FlexArgsTest
+//
+//  Created by Kyle Isom on 15/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FlexArgsTest : SenTestCase
+
+@end
---
+++ FlexArgsTest/FlexArgsTest.m
@@ -0,0 +1,48 @@
+//
+//  FlexArgsTest.m
+//  FlexArgsTest
+//
+//  Created by Kyle Isom on 15/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlexArgsTest.h"
+
+@implementation FlexArgsTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)test_init_with_NSArray
+{
+    NSArray *testArgs = [NSArray arrayWithObjects:
+                         @"foo=bar",
+                         @"baz=1",
+                         @"quux=-2.5",
+                         @"spam=footastic",
+                         @"eggs=false", nil];
+    NSDictionary *expected = [NSDictionary dictionaryWithObjectsAndKeys:
+                              @"bar", @"foo",
+                              [NSNumber numberWithLongLong:1], @"baz",
+                              [NSNumber numberWithDouble:-2.5], @"quux",
+                              @"footastic", @"spam",
+                              [NSNumber numberWithBool:false], @"eggs", nil];
+    FlexArgs *flexArgs = [[FlexArgs alloc] initParserWithNSArray:testArgs];
+    NSDictionary *parsed = [flexArgs retrieveArgs];
+
+    STAssertTrue([parsed isEqualToDictionary:expected], 
+                 @"initParserWithNSArray failed to return the expected NSDictionary!");
+}
+
+@end
